8e33eec504dce402e4651a16b390413dcdc29140
Maxime Larronde <mlarronde@iutbayonne.univ-pau.fr>
2018-11-28 22:15:42 +0100

    Mise en pace d'une page d'accueil très simple

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -12,8 +12,6 @@ class ProStageController extends AbstractController
      */
     public function index()
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
-        ]);
+        return $this->render('pro_stage/index.html.twig');
     }
 }
