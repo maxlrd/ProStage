d90095fb662c0095202c1a5dc993c250de9dd106
Maxime Larronde <mlarronde@iparla.iutbayonne.univ-pau.fr>
2019-01-03 13:56:04 +0100

    Ajout des stages dans la bd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,12 +14,39 @@ class AppFixtures extends Fixture
     {
     	$faker = \Faker\Factory::create('fr_FR'); // create a French faker
 
-    	/**********************************************
+        /**********************************************
+    	***          Création des Entreprises       ***
+    	***********************************************/
+
+    	$nbEntreprises = 10;
+        $tabEntreprise = array();
+
+        for ($i=0; $i < $nbEntreprises; $i++) { 
+            
+            $entreprise = new Entreprise();
+
+            //Ajout d'un nom pour l'entreprise via la library faker
+            $entreprise->setNom($faker->company());
+            //Ajout d'une adresse pour l'entreprise via la library faker
+            $entreprise->setAdresse($faker->streetAddress());
+            //Ajout d'une activitée pour l'entreprise via la library faker
+            $entreprise->setActivite($faker->jobTitle());
+            //Ajout d'une ville pour l'entreprise via la library faker
+            $entreprise->setVille($faker->city());
+            //Ajout d'une activitée pour l'entreprise via la library faker
+            $entreprise->setCp((int)$faker->postcode());
+
+            $tabEntreprise[$i] = $entreprise;
+
+            $manager->persist($entreprise);
+        }
+
+        /**********************************************
     	***          Création des Formations        ***
     	***********************************************/
 
     	//Création de la Formation Informatique
-        $formationInfo = new Formation();
+        /*$formationInfo = new Formation();
         $formationInfo->setNom("Informatique");
         $manager->persist($formationInfo);
 
@@ -36,35 +63,53 @@ class AppFixtures extends Fixture
         //Création de la Formation Génie Industriel et Maintenance
         $formationGIM = new Formation();
         $formationGIM->setNom("Génie Industriel et Maintenance");
-        $manager->persist($formationGIM);
+        $manager->persist($formationGIM);*/
 
-        /**********************************************
-    	***          Création des Entreprises       ***
-    	***********************************************/
-
-    	$nbEntreprises = 10;
-
-    	for ($i=0; $i < $nbEntreprises; $i++) { 
-    		
-    		$entreprise = new Entreprise();
-
-    		//Ajout d'un nom pour l'entreprise via la library faker
-    		$entreprise->setNom($faker->company());
-    		//Ajout d'une adresse pour l'entreprise via la library faker
-    		$entreprise->setAdresse($faker->streetAddress());
-    		//Ajout d'une activitée pour l'entreprise via la library faker
-    		$entreprise->setActivite($faker->jobTitle());
-    		//Ajout d'une ville pour l'entreprise via la library faker
-    		$entreprise->setVille($faker->city());
-    		//Ajout d'une activitée pour l'entreprise via la library faker
-    		$entreprise->setCp((int)$faker->postcode());
-
-    		$manager->persist($entreprise);
-    	}
+        $tabFormations = array(
+        	"Info" => "Informatique",
+        	"ProgAv" => "Programmation Avancée",
+        	"MultiMedia" => "MultiMédia",
+        	"GIM" => "Génie Industriel et Maintenance"
+        );
 
     	/**********************************************
-    	***          Création des Stages            ***
+    	***   Création des Stages avec Formations   ***    
     	***********************************************/
+
+    	foreach ($tabFormations as $codeFormation => $nomFormation) {
+
+    		//Création des formations
+    		$formation = new Formation();
+        	$formation->setNom("$nomFormation");
+        	$manager->persist($formation);
+    		
+    		//Création de plusieurs stages associés aux entreprises
+    		$nbStagesAGenerer = $faker->numberBetween($min = 0, $max = 9);
+
+    		for ($numStage=0; $numStage < $nbStagesAGenerer ; $numStage++) { 
+    			
+    			$stage = new Stages();
+    			$stage->setTitre($faker->sentence($nbWords = 5, $variableWords = True));
+    			$stage->setDescription($faker->realText($maxNbChars = 500, $indexSize = 2));
+    			$stage->setEmail($faker->freeEmail());
+    			
+    			//Création d'une relation Stages --> Formation
+    			$stage->addFormation($formation);
+
+    			//Sélectionner une entreprise parmis les 10 générées
+    			$numEntreprise = $faker->numberBetween($min = 0, $max = 9);
+
+    			//Création d'une relation Stages --> Entreprise
+    			$stage->setEntreprise($tabEntreprise[$numEntreprise]);
+
+    			//Création d'une relation Entreprise --> Stages
+    			$tabEntreprise[$numEntreprise] -> addStage($stage);
+
+    			//Persister les objets modifiés (Stages et Entreprise)
+    			$manager->persist($stage);
+    			$manager->persist($tabEntreprise[$numEntreprise]);
+    		}
+    	}
 
         //Envoie des données dans la base de donnée
         $manager->flush();
