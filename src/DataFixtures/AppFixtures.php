12d26eeffe9fb30f929b5e051a414cf973639ec7
Maxime Larronde <mlarronde@iparla.iutbayonne.univ-pau.fr>
2019-01-03 11:15:34 +0100

    Ajout de toutes les formations dans la bd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,11 +10,27 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+    	//Création de la Formation Informatique
         $formationInfo = new Formation();
         $formationInfo->setNom("Informatique");
-
         $manager->persist($formationInfo);
 
+        //Création de la Formation Programmation Avancée
+        $formationProgAv = new Formation();
+        $formationProgAv->setNom("Programmation Avancée");
+        $manager->persist($formationProgAv);
+
+        //Création de la Formation MultiMédia
+        $formationMultiMedia = new Formation();
+        $formationMultiMedia->setNom("MultiMédia");
+        $manager->persist($formationMultiMedia);
+
+        //Création de la Formation Génie Industriel et Maintenance
+        $formationGIM = new Formation();
+        $formationGIM->setNom("Génie Industriel et Maintenance");
+        $manager->persist($formationGIM);
+
+        //Envoie des données dans la base de donnée
         $manager->flush();
     }
 }
